a54b490755ffbefc1fa5fb1e24fb3fdfb2377bf5
Николаенков Михаил Дмитриевич <iv222s16@lwp11.csc.local>
2023-02-14 11:07:15 +0700

    Add simple reading file

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argv, char** argc)
 {
-    printf("Hello World!\n");
+    for (int i = 1; i < argv; i++) {
+        printf("%s\n", argc[i]);
+    }
+    FILE* file = fopen("commands", "r");
+    if (file == NULL) {
+        printf("Error: can't open commands file");
+        return -1;
+    }
+    int count = 0;
+
+    char bufStr[50][500];
+    fgets(bufStr[count], 500, file);
+
+    while (bufStr[count][0] != '\0') {
+        count += 1;
+        fgets(bufStr[count], 500, file);
+        printf("%s", bufStr[count]);
+
+    }
+
+    fclose(file);
     return 0;
 }
