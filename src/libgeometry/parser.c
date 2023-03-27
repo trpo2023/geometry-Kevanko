b60ddc7b3069c375ca09f6570253c3b628e20748
Misha <mr_vidali@mail.ru>
2023-03-28 00:37:37 +0700

    Parser.c change parse_circle

--- src/libgeometry/parser.c
+++ src/libgeometry/parser.c
@@ -1,9 +1,10 @@
-#include <libgeometry/parser.h>
-
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgeometry/parser.h>
+
+#define M_PI 3.14
 
 // Функция для обработки ошибок и вывода сообщения
 void handle_error(const char* error_msg, const char* input, int pos)
@@ -50,15 +51,15 @@ void print_result(
            area);
 }
 // Функция для парсинга круга
-
-Circle* parse_circle(char* input)
+Circle parse_circle(char* input)
 {
+    Circle exit_error = {0.f, 0.f, 0.f, 0.f, 0.f, 0};
     const char* prefix = "circle(";
     char* start_ptr = input;
     int prefix_len = strlen(prefix);
     if (strncmp(input, prefix, prefix_len)) {
         handle_error("Error: expected '('", start_ptr, 7);
-        return NULL;
+        return exit_error;
     }
     input += prefix_len;
     char* end_ptr;
@@ -66,32 +67,32 @@ Circle* parse_circle(char* input)
     if (end_ptr == input || *end_ptr != ' ') {
         handle_error(
                 "Error: expected <double>", start_ptr, end_ptr - start_ptr);
-        return NULL;
+        return exit_error;
     }
     input = end_ptr + 1;
     double y = strtod(input, &end_ptr);
     if (end_ptr == input || *end_ptr != ',') {
         handle_error("Error: expected ','", start_ptr, end_ptr - start_ptr);
-        return NULL;
+        return exit_error;
     }
     input = end_ptr + 1;
     double radius = strtod(input, &end_ptr);
     if (end_ptr == input || *end_ptr != ')') {
         handle_error("Error: expected ')'", start_ptr, end_ptr - start_ptr);
-        return NULL;
+        return exit_error;
     }
     end_ptr += 1;
     if (*end_ptr != '\0') {
         handle_error("Error: unexpected token", start_ptr, end_ptr - start_ptr);
-        return NULL;
+        return exit_error;
     }
-    Circle* result = (Circle*)malloc(sizeof(Circle));
-    result->x = x;
-    result->y = y;
-    result->r = radius;
-    result->perimetr = 2 * 3.14 * result->r;
-    result->area = 3.14 * result->r * result->r;
-
+    Circle result
+            = {.x = x,
+               .y = y,
+               .r = radius,
+               .perimetr = 2 * M_PI * radius,
+               .area = M_PI * radius * radius,
+               .exist = 1};
     return result;
 }
 // Функция для обработки входной строки
@@ -99,16 +100,16 @@ int parse_input(char* input, const char* output_path)
 {
     input = lower_all(input);
     if (strncmp(input, "circle", 6) == 0) {
-        Circle* circle = parse_circle(input);
-        if (circle) {
+        Circle circle = parse_circle(input);
+        if (circle.exist) {
             char name[50];
             sprintf(name,
                     "circle(%.1f %.1f %.1f)",
-                    circle->x,
-                    circle->y,
-                    circle->r);
+                    circle.x,
+                    circle.y,
+                    circle.r);
 
-            print_result(output_path, name, circle->perimetr, circle->area);
+            print_result(output_path, name, circle.perimetr, circle.area);
             return 0;
         }
     }
