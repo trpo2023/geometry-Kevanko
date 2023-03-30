1522167a6fb6ea83e5c9217710b2e0aca8299533
Master <mr_vidali@mail.ru>
2023-03-30 14:52:15 +0700

    Change parser_circle

--- src/libgeometry/parser.h
+++ src/libgeometry/parser.h
@@ -3,17 +3,30 @@
 
 typedef struct Circle {
     double x, y, r, perimetr, area;
-    int exist;
 } Circle;
 
+enum ERRORS_ID {
+    ERROR_FILE_OUTPUT = 1,
+    ERROR_FILE_INPUT,
+    ERROR_PARSER_NAME,
+    ERROR_PARSER_LEFT_PARENTHESIS,
+    ERROR_PARSER_RIGHT_PARENTHESIS,
+    ERROR_PARSER_COMMA,
+    ERROR_PARSER_DOUBLE,
+    ERROR_PARSER_UNEXPECTED_TOKEN,
+};
+int is_input_files_exist(const char* input_path, const char* output_path);
 // Функция для обработки ошибок и вывода сообщения
-void handle_error(const char* error_msg, const char* input, int pos);
+void handle_error(int error_id, char* str);
 // Функция для перевода строки в нижний регистр
 char* lower_all(char* str);
 // Функция для парсинга круга
-Circle parse_circle(char* input);
+int is_circle(char* str);
+int is_left_parenthesis(char* str);
+int is_x_circle(char* str, double* x);
+int is_y_circle(char* str, double* y);
+int is_r_circle(char* str, double* r);
+int parse_circle(char* str, Circle* out_values);
 // Функция для обработки входной строки
 int parse_input(char* input, const char* output_file);
-
-void print_result(
-        const char* output_path, char* name, double perimetr, double area);
+void print_circle(const char* output_path, Circle* circle);
